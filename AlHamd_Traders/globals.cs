e610c8a414b4d6398411de31f195a0598a37d994
Farook M Fazrin <nirzaf1@hotmail.com>
2020-07-01 23:46:20 +0530

    fgfg

--- AlHamd_Traders/globals.cs
+++ AlHamd_Traders/globals.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlHamd_Traders
 {
-    class globals
+    class Globals
     {
         public static String username;
         public static int userid;
